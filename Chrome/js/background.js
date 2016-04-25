b55ac0b6224c42469e233bcd7efe85a6d88636e9
onefourtwoeightfiveseven <brian@briananderson.co.uk>
2016-04-25 05:14:06 +0100

    Update tab icon on tab activation
    When a post is deleted from Pinboard, it's necessary to update the icon upon tab focus.

--- Chrome/js/background.js
+++ Chrome/js/background.js
@@ -1,38 +1,43 @@
 (function (Pinboard, chrome, window) {
   'use strict';
+  chrome.tabs.onActivated.addListener(onTabActivated);
   chrome.tabs.onUpdated.addListener(onTabUpdated);
 
   window.savedPosts = {};
 
-  function onTabUpdated(tabId, changeInfo, tab) {
+  function onTabActivated(activeInfo){
+    chrome.tabs.get(activeInfo.tabId, tab => {
+      pinboardCheck(tab);
+    });
+  }
 
+  function onTabUpdated(tabId, changeInfo, tab) {
+    chrome.pageAction.show(tabId);
+    pinboardCheck(tab);
+  }
+
+  // Check if there's a Pinboard entry for a given tab
+  function pinboardCheck(tab){
     if (!tab.url.match(/^http/)){
       return;
     }
 
     chrome.storage.sync.get(["apitoken"], data => {
-
       if (!data || !data.hasOwnProperty("apitoken")) {
         console.log("No Pinboard API token found");
         return;
       }
-
-      // Check if this tab URL is saved in user's Pinboard
-      var pinboard = new Pinboard(data.apitoken);
+      let pinboard = new Pinboard(data.apitoken);
       pinboard.posts.get({ url : tab.url }).then(data => {
         if (data.posts.length > 0) {
-          // URL is in user's Pinboard
           showActiveIcon(tab.id);
           window.savedPosts[tab.url] = data.posts[0];    // Cache the post info incase the user clicks the popup
         } else {
-          // URL is not in user's pinboard
+          showDeactiveIcon(tab.id);
           delete window.savedPosts[tab.url];             // Clear any cache entry for this post
         }
       });
-
-    });
-
-    chrome.pageAction.show(tabId);
+    });    
   }
 
   function showActiveIcon(tabId){
